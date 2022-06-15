e5714422d047cb27cf64f3c3048111728abbfdc7
Yaroslav Boiko <slkawnia@gmail.com>
2022-06-15 18:02:49 +0300

    Draw №1. July 4, 2022 Test

---
+++ js/config/win_prizes.js
@@ -0,0 +1,17 @@
+const confPrizes = {40: 250, 50: 500, 54: 1250};
+let winPrizes = {};
+
+for(let i = 0; i < Object.keys(confPrizes).length; i++) {
+    createWinPrizeList(Object.keys(confPrizes)[i - 1], Object.keys(confPrizes)[i], Object.values(confPrizes)[i]);
+}
+
+console.log(winPrizes);
+
+function createWinPrizeList (from, to, value) {
+    from = from === undefined? 0 : parseInt(from);
+    to = parseInt(to);
+
+    for(let i = from; i < to; i++) {
+        winPrizes[i + 1] = value;
+    }
+}
